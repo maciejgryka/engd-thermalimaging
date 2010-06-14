59982f286d8e0c68797f7ecbc828c5abb1f41614
maciej.gryka <maciej.gryka@64e43b4a-0204-9413-cf74-55b705463e3c>
2010-06-14 17:41:04 +0000

    now uses one big texture nicely mapped for many objects

--- ThermalImaging/ThermalImaging/Ply2OpenGL.h
+++ ThermalImaging/ThermalImaging/Ply2OpenGL.h
@@ -1,37 +1,82 @@
 #ifndef PLY2OPENGL
 #define PLY2OPENGL
 
-#define NOMINMAX
+#include <iostream>
 
-#include <QFile>
+
+
 #include <QString>
 #include <QStringList>
+#include <QFile>
 #include <QTextStream>
-#include <QFileDialog>
 
-#include <iostream>
+
+
 
 class Ply2OpenGL {
 
-private:
-	int nPoints;
-	QString inputFile;
-	float *points;
-	float *normals;
-	float *colors;
-	
 public:
-	Ply2OpenGL();
-	~Ply2OpenGL();
-	void setUp();
-	void setFile();
-	int createFiles();
-	int getNumberOfPoints();
-	float *getPoints();
-	float *getNormals();
-	float *getColors();
-	float **convert2Matrix(float*);
+	Ply2OpenGL(){};
+	~Ply2OpenGL()
+	{
+		delete vertices;
+		delete indices;
+		delete texCoords;
+		//delete colors;
+		//delete normals;
+	};
 
+	bool readPlyFile(QString filename);
+	
+	float* getVertices() { return vertices;	};
+
+	int* getIndices() { return indices; };
+	
+	float* getTexCoords() { return texCoords; };
+
+	int getNVertices() { return nVertices; };
+
+	int getNFaces() { return nFaces; };
+
+	int getVertexSize() { return vertexSize; };
+
+	int getVerticesPerFace() { return verticesPerFace; };
+
+	int getTexCoordSize() { return texCoordSize; };
+
+	//float* getColors();
+	//float* getNormals();
+
+private:
+
+
+
+
+
+	
+	float* vertices;
+	int* indices;
+	float* texCoords;
+	//float* colors;
+	//float* normals;
+
+	float* vertices;
+	int* indices;
+	float* texCoords;
+
+	//float* colors;
+	//float* normals;
+
+	int nVertices;
+	int nFaces;
+	
+	static const int vertexSize = 3;
+	static const int verticesPerFace = 3;
+	static const int texCoordSize = 2;
+	//static const int colorSize = 3;
+	//static const int normalSize = 3;
+
+	QString getL(QTextStream &stream);
 };
 
 #endif
