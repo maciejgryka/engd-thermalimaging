39cf66ef3facc2f22ce82b0b1795af74f7c9a613
fabian.wanner <fabian.wanner@64e43b4a-0204-9413-cf74-55b705463e3c>
2010-06-21 10:40:00 +0000

    Added Corners to the class

--- ThermalImaging/ThermalImaging/PlaneInfo.h
+++ ThermalImaging/ThermalImaging/PlaneInfo.h
@@ -13,56 +13,65 @@ USING_PART_OF_NAMESPACE_EIGEN
 using namespace std;
 
 class PlaneInfo {
+	//this class contains all the information relevant to a plane
+	// it stores all this information together and also allows writing this information to a file or reading it from one
 
 private:
-	Vector3f normal;
+	Vector3f* normal; // the normal of the plane
 	
-	Vector3f translationVector;
-	Matrix3f rotationMatrix;
+	Vector3f* translationVector; // the translation to the origin
+	Matrix3f* rotationMatrix; // the rotation matrix that rotates the plane into the XZ plane
 
-	float** pointsOnPlane;
+	float** pointsOnPlane; // the xyz coordinates of all the points on the plane
 
-	float* color;
+	float* color; // the color of the plane (currently the plane is one color
 
-	int* pointsUsed;
-	int numberOfPoints;
+	int* pointsUsed; // the indices of the points on the plane
+	int numberOfPoints; // the number of Points on the plane
 
-	Quad* quad;
-	Grid* grid;
+	Quad* quad; // the quad, which was used to find the approximate area covered by the plane
+	Grid* grid; // the grid obtained from the quad, so that dilation, erosion and border detection can be performed
 
-	vector<float> xBorder;
-	vector<float> yBorder;
-	vector<float> zBorder;
+	vector<float>* xBorder; // the x coordinates of the border
+	vector<float>* yBorder; // the y coordinates of the border
+	vector<float>* zBorder; // the z coordinates of the border
+
+	vector<vector<float>>* corners;
 
 public:
 	PlaneInfo();
 	~PlaneInfo();
 
-	Vector3f getNormal();
-	Vector3f getTranslationVector();
-	Matrix3f getRotationMatrix();
+	Vector3f* getNormal();
+	Vector3f* getTranslationVector();
+	Matrix3f* getRotationMatrix();
 	float* getColor();
 	int* getPointsUsed();
 	int getPointNumber();
 	Quad* getQuad();
 	Grid* getGrid();
 	float** getPointsOnPlane();
-	vector<float> getXBorder();
-	vector<float> getYBorder();
-	vector<float> getZBorder();
+	vector<float>* getXBorder();
+	vector<float>* getYBorder();
+	vector<float>* getZBorder();
+	vector<vector<float>>* getCorners();
 
-	void setNormal(Vector3f);
-	void setTranslationVector(Vector3f);
-	void setRotationMatrix(Matrix3f);
+	void setNormal(Vector3f*);
+	void setTranslationVector(Vector3f*);
+	void setRotationMatrix(Matrix3f*);
 	void setColor(float*);
 	void setPointsUsed(int*);
 	void setPointNumber(int);
 	void setQuad(Quad*);
 	void setGrid(Grid*);
 	void setPointsOnPlane(float**);
-	void setXBorder(vector<float>);
-	void setYBorder(vector<float>);
-	void setZBorder(vector<float>);
+	void setXBorder(vector<float>*);
+	void setYBorder(vector<float>*);
+	void setZBorder(vector<float>*);
+	void setCorners(vector<vector<float>>*);
+
+	void writePlane(QString);
+	void readPlane(QString);
 
 };
 
