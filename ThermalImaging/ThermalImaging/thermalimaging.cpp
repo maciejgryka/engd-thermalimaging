2b672cef0374f9a0d7de3f21336a8e16f4df75a4
fabian.wanner <fabian.wanner@64e43b4a-0204-9413-cf74-55b705463e3c>
2010-06-30 13:21:23 +0000

    New slider and button slots and signals

--- ThermalImaging/ThermalImaging/thermalimaging.cpp
+++ ThermalImaging/ThermalImaging/thermalimaging.cpp
@@ -5,7 +5,23 @@ ThermalImaging::ThermalImaging(QWidget *parent, Qt::WFlags flags)
 {
 	ui.setupUi(this);
 	//connect(ui.thermalVisualSlider,SIGNAL(sliderMoved(int)),ui.mainWid,SLOT(thermalVisualPercent(int)));
-	connect(ui.thermalVisualSlider,SIGNAL(sliderMoved(int)),ui.lcdNumber,SLOT(display(int)));
+	//connect(ui.thermalVisualSlider,SIGNAL(sliderMoved(int)),ui.lcdNumber,SLOT(display(int)));
+
+	//connect(ui.clusterSlider,SIGNAL(sliderMoved(int)),ui.mainWid,SLOT(cluster(int)));
+	//connect(ui.clusterSlider,SIGNAL(sliderMoved(int)),ui.clusterView,SLOT(display(int)));
+
+	
+
+	connect(ui.quadSlider,SIGNAL(sliderMoved(int)),ui.mainWid,SLOT(setMaxLevels(int)));
+	connect(ui.quadSlider,SIGNAL(sliderMoved(int)),ui.quadView,SLOT(setNum(int)));
+	connect(ui.quadButton,SIGNAL(clicked()),ui.mainWid,SLOT(rerunQuad()));
+
+	connect(ui.edgeSlider,SIGNAL(sliderMoved(int)),ui.mainWid,SLOT(setEdgeInlier(int)));
+	connect(ui.edgeSlider,SIGNAL(sliderMoved(int)),ui.edgeView,SLOT(setNum(int)));
+
+	connect(ui.chillSlider,SIGNAL(sliderMoved(int)),ui.mainWid,SLOT(setChillPoints(int)));
+	connect(ui.chillSlider,SIGNAL(sliderMoved(int)),ui.chillView,SLOT(setNum(int)));
+	connect(ui.rpeButton,SIGNAL(clicked()),ui.mainWid,SLOT(rerunRPE()));
 	//connect(ui.ransacButton,SIGNAL(clicked()),ui.mainWid,SLOT(executeRansac()));
 	ui.thermalVisualSlider->show();
 	ui.mainWid->show();
